61bfdcd6c0b8fde390582940f5afb68c29292f35
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-08-29 12:49:03 +0200

    user list component

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  //TODO: will fetch from db later
+  const USERS = [
+    {
+      id: "u1",
+      name: "Pizza",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeilFR4pfeMxzs0atfZMY-50F1ZYVc8I5ynAqDlfRh3TUfTPJqRim7FUYOig&s",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
