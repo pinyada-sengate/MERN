af115141c4eca4893d56666e95ae03def6b16d59
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-08-29 19:21:01 +0200

    navlinks

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 
 import "./MainNavigation.css";
 
@@ -16,7 +17,9 @@ const MainNavigation = (props) => {
       <h1 className="main-navigation__title">
         <Link to="/">YourPlaces</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
