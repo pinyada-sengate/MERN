b529ca97163ed4b71d1e2d489b4cfb0d9a0e19cc
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-08-30 15:54:55 +0200

    useParam to get user id

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,40 +1,44 @@
 import React from "react";
 import PlaceList from "../components/PlaceList";
+import { useParams } from "react-router-dom";
+
+// TODO: will fetch from database later
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Basílica de la Sagrada Família",
+    description:
+      "Antoni Gaudí's renowned unfinished church, started in the 1880s, with museum and city views.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipPvcLglf3S0-gNAZEVATnRTaObdbR4fcclktCO0=w408-h306-k-no",
+    address: "Eixample, 08013 Barcelona",
+    location: {
+      lat: 41.403706,
+      lng: 2.173504,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Basílica de la Sagrada Família",
+    description:
+      "Antoni Gaudí's renowned unfinished church, started in the 1880s, with museum and city views.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipPvcLglf3S0-gNAZEVATnRTaObdbR4fcclktCO0=w408-h306-k-no",
+    address: "Eixample, 08013 Barcelona",
+    location: {
+      lat: 41.403706,
+      lng: 2.173504,
+    },
+    creator: "u2",
+  },
+];
 
 const UserPlaces = () => {
-  // TODO: will fetch from database later
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
 
-  const DUMMY_PLACES = [
-    {
-      id: "p1",
-      title: "Basílica de la Sagrada Família",
-      description:
-        "Antoni Gaudí's renowned unfinished church, started in the 1880s, with museum and city views.",
-      imageUrl:
-        "https://lh5.googleusercontent.com/p/AF1QipPvcLglf3S0-gNAZEVATnRTaObdbR4fcclktCO0=w408-h306-k-no",
-      address: "Eixample, 08013 Barcelona",
-      location: {
-        lat: 41.403706,
-        lng: 2.173504,
-      },
-      creator: "u1",
-    },
-    {
-      id: "p2",
-      title: "Basílica de la Sagrada Família",
-      description:
-        "Antoni Gaudí's renowned unfinished church, started in the 1880s, with museum and city views.",
-      imageUrl:
-        "https://lh5.googleusercontent.com/p/AF1QipPvcLglf3S0-gNAZEVATnRTaObdbR4fcclktCO0=w408-h306-k-no",
-      address: "Eixample, 08013 Barcelona",
-      location: {
-        lat: 41.403706,
-        lng: 2.173504,
-      },
-      creator: "u2",
-    },
-  ];
-  return <PlaceList items={DUMMY_PLACES} />;
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
