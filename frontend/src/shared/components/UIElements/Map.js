948e217af0eed8e80afb83a40ebff2c12f6fa4d7
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-09-02 21:04:29 +0200

    map component

---
+++ frontend/src/shared/components/UIElements/Map.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./Map.css";
+
+const Map = (props) => {
+  //TODO: implement map later
+  return <div className={`map ${props.className}`} style={props.style}></div>;
+};
+
+export default Map;
