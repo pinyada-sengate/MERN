4eaa4cf0e7ff8d119e90f688a9ab49105252c942
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-09-05 10:00:38 +0200

    add mutiple input

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import "./NewPlace.css";
 
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter valid title"
+        onInput={titleInputHandler}
+      />
+
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter valid description (at least 5 characters)"
+        onInput={descriptionInputHandler}
       />
     </form>
   );
