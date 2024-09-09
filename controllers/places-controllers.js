756497d3d6b8a9026d07140fb09fe77a931a7d9b
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-09-09 08:15:32 +0200

    update place api

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -77,13 +77,35 @@ const createPlace = (req, res, next) => {
   };
 
   // will add to database later
-  DUMMY_PLACES.push(createPlace);
+  DUMMY_PLACES.push(createdPlace);
 
   res.status(201).json({
     place: createdPlace,
   });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+
+  if (!updatedPlace) {
+    throw new HttpError("Could not find a place for the provided id", 404);
+  }
+
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({
+    place: updatedPlace,
+  });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
