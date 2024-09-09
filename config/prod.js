67edd8c617f6779498e7419e27206147cd82f40f
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-09-09 15:55:02 +0200

    add dev and prod key files

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+// Production Environment
+module.exports = {
+  googleMapAPIKey: process.env.GOOGLE_MAP_API_KEY,
+};
---
+++ util/location.js
@@ -0,0 +1,7 @@
+//dummy function
+function getCoordsForAddress(address) {
+  return {
+    lat: 41.403706,
+    lng: 2.173504,
+  };
+}
