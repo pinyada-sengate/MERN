e3a943b891a4394b8b6dad07acddc8923d8e335d
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-10-01 11:46:37 +0200

    frontend implement fetch places by user id

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,44 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlaceList from "../components/PlaceList";
 import { useParams } from "react-router-dom";
-
-// TODO: will fetch from database later
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Basílica de la Sagrada Família",
-    description:
-      "Antoni Gaudí's renowned unfinished church, started in the 1880s, with museum and city views.",
-    imageUrl:
-      "https://lh5.googleusercontent.com/p/AF1QipPvcLglf3S0-gNAZEVATnRTaObdbR4fcclktCO0=w408-h306-k-no",
-    address: "Eixample, 08013 Barcelona",
-    location: {
-      lat: 41.403706,
-      lng: 2.173504,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Basílica de la Sagrada Família",
-    description:
-      "Antoni Gaudí's renowned unfinished church, started in the 1880s, with museum and city views.",
-    imageUrl:
-      "https://lh5.googleusercontent.com/p/AF1QipPvcLglf3S0-gNAZEVATnRTaObdbR4fcclktCO0=w408-h306-k-no",
-    address: "Eixample, 08013 Barcelona",
-    location: {
-      lat: 41.403706,
-      lng: 2.173504,
-    },
-    creator: "u2",
-  },
-];
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const UserPlaces = () => {
-  const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  return <PlaceList items={loadedPlaces} />;
+  const userId = useParams().userId;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
+  );
 };
 
 export default UserPlaces;
