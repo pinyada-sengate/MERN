8b3793403e5e817b754d19d74facff2972d38481
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-10-07 11:27:41 +0200

    backend add auth middleware

---
+++ backend/middleware/check-auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const HttpError = require("../models/http-error");
+const keys = require("../config/keys");
+
+module.exports = (req, res, next) => {
+  try {
+    // Authorization: "Barer token"
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new Error("Authentication failed");
+    }
+
+    const decodedToken = jwt.verify(token, keys.tokenPrivateKey);
+    req.userData = { userId: decodedToken.userId };
+  } catch (err) {
+    const error = new HttpError("Authentication failed!", 401);
+    return next(error);
+  }
+};
