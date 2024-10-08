2f90cdb87ab9fb69f001385dc35f54520f295674
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-10-08 10:50:07 +0200

    frontend using token for update and delete place

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -79,6 +79,7 @@ const UpdatePlace = () => {
         }),
         {
           "Content-Type": "application/json",
+          Authorization: "Bearer " + auth.token,
         }
       );
       history.push("/" + auth.userId + "/places");
